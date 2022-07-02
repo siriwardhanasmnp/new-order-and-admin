abb222502759caebf53025f4fdb21bb8f70acc20
pabasararanasinghe <ranasinghemmpn.19@uom.lk>
2022-07-02 13:25:32 +0530

    Admin Login design -M

--- src/pages/AdminSignIn/AdminSignIn.js
+++ src/pages/AdminSignIn/AdminSignIn.js
@@ -4,8 +4,9 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
-import Admin from '../../resources/images/admin.jpg'
+import Admin from '../../resources/images/adminImg.png'
 import { width } from '@mui/system';
+import {Link} from 'react-router-dom';
 
 
   const onFinish = (values) => 
@@ -18,10 +19,11 @@ function AdminSignIn() {
   <>
     <div className='adminDiv'> 
        <React.Fragment>
-      <CssBaseline />
-      <Container className='adminDiv' maxWidth="sm">
+        <div style={{height:'40px'}}></div>
+      
+      <Container className='adminDiv' style={{height:'620px', width: '600px', backgroundColor:'#DCDCDC',  borderRadius:'8px'}}>
       <Box sx={{ bgcolor: ' rgb(24, 24, 24', height: '95vh' }}>
-        <img src={Admin} width={350} height={350} />
+        <img style={{marginLeft:'170px'}} src={Admin} width={250} height={250} />
       <Form
       name="normal_login"
       className="login-form"
@@ -30,6 +32,8 @@ function AdminSignIn() {
       }}
       onFinish={onFinish}
     >
+
+{/* Admin User Name */}
       <Form.Item
         name="username"
         rules={[
@@ -39,8 +43,12 @@ function AdminSignIn() {
           },
         ]}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        <Input 
+        style={{width: '400px', height:'50px', margin:'60px 0px 0px 100px', borderRadius:'8px'}}
+        prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
       </Form.Item>
+
+      {/* Password */}
       <Form.Item
         name="password"
         rules={[
@@ -51,11 +59,14 @@ function AdminSignIn() {
         ]}
       >
         <Input
+          style={{width: '400px', height:'50px', margin:'10px 0px 0px 100px', borderRadius:'8px'}}
           prefix={<LockOutlined className="site-form-item-icon" />}
           type="password"
           placeholder="Password"
         />
       </Form.Item>
+
+      <div style={{float:'right'}}>
       <Form.Item>
         <Form.Item name="remember" valuePropName="checked" noStyle>
           <Checkbox>Remember me</Checkbox>
@@ -65,13 +76,17 @@ function AdminSignIn() {
           Forgot password
         </a>
       </Form.Item>
+      </div>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
+      <div style={{margin:'80px 0px 0px 215px '}}>
+        <Form.Item>
+          <Link to='/portal'>
+          <Button type="primary" htmlType="submit" className="login-form-button" style={{height:'50px', width:'100px', borderRadius:'10px'}}>
+           <h4 style={{color:'white'}}> Log in</h4>
+          </Button>
+          </Link>
+        </Form.Item>
+      </div>
     </Form>
     </Box>
        
