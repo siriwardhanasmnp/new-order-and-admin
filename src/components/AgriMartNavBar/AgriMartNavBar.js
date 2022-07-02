b96229f7af871ffe81ae9492a637c2dde295d4fe
pabasararanasinghe <ranasinghemmpn.19@uom.lk>
2022-07-02 23:47:31 +0530

    Nav bar design - edited

--- src/components/AgriMartNavBar/AgriMartNavBar.js
+++ src/components/AgriMartNavBar/AgriMartNavBar.js
@@ -4,19 +4,22 @@ import { Navbar, Nav, Container, Badge, Image } from 'react-bootstrap';
 import './AgriMartNavBar.css';
 import logo from '../../resources/images/logo.png'
 import { UserOutlined } from '@ant-design/icons';
-import { Avatar } from 'antd';
+import { Avatar, Row, Col } from 'antd';
 function AgriMartNavBar() {
     return (
-        <Navbar class="nav-styles" collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+        <Navbar class="nav-styles" collapseOnSelect fixed="top" expand="sm" style={{backgroundColor:'#001a00'}}>
             <Container>
                 <Navbar.Brand href="/">
-                <Image src={logo} height={70} width={70} />
-                 Agri Mart   </Navbar.Brand>
+              <Row justify='space-around'>   
+                <Image src={logo} height={80} width={80} />
+                <h3 style={{color:'#e6ffe6', paddingLeft:'20px'}}>Agri Mart  </h3> 
+            </Row>
+                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                     <Nav.Link href='/'>Farmer Portal</Nav.Link>
-                        <Nav.Link href="/aboutus">About Us</Nav.Link>
+                    <Nav className="me-auto" >
+                     <Nav.Link href='/'> <h5 style={{color:'#e6ffe6', paddingLeft:'50px', fontSize:'16px'}}>Farmer Portal</h5></Nav.Link>
+                        <Nav.Link href="/aboutus"><h5 style={{color:'#e6ffe6', paddingLeft:'20px', fontSize:'16px'}}>About Us</h5></Nav.Link>
                     </Nav>
                     <Nav>
                     <Nav.Link href='/login'>Sign in</Nav.Link>
